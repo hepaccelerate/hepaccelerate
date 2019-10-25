331d093da23eed2c7124758559eed548abde918d
Joosep Pata <joosep.pata@gmail.com>
2019-10-24 19:21:03 -0700

    bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/hepaccelerate/hepaccelerate'
 EMAIL = 'joosep.pata@cern.ch'
 AUTHOR = 'Joosep Pata'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.2.1'
+VERSION = '0.2.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
